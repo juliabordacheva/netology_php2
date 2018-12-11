5ab10daa41aff20cd9ff855af5995480a207d3b1
Юлия Бордачева <s_proza@list.ru>
2018-12-11 19:07:13 +0300

    Fourth commit

--- about.php
+++ about.php
@@ -6,25 +6,18 @@
     </head>
     <body>
       <h1>Знакомство с PHP</h1>
-    </body>
-  </html>
-
-
-
-<?php header("Content-Type: text/html; charset=utf-8");
-    $userName = 'Юлия';
-    $userAge = '36';
-    $mail = 'yulia@mail.ru';
-    $city = 'Moscow';
-    $about = 'Продавщица в Ашане';
-    ?>
-
-
-    <p>Имя: <strong><?= $userName ?></strong></p>
-    <p>Возраст: <strong><?= $userAge ?></strong></p>
-    <p>Электронная почта: <strong><?= $mail ?></strong></p>
-    <p>Город: <strong><?= $city ?></strong></p>
-    <p>О себе: <strong><?= $about ?></strong></p>
+      <?php 
+          $userName = 'Юлия';
+          $userAge = '36';
+          $mail = 'yulia@mail.ru';
+          $city = 'Moscow';
+          $about = 'Продавщица в Ашане';
+          ?>
+      <p>Имя: <strong><?= $userName ?></strong></p>
+      <p>Возраст: <strong><?= $userAge ?></strong></p>
+      <p>Электронная почта: <strong><?= $mail ?></strong></p>
+      <p>Город: <strong><?= $city ?></strong></p>
+      <p>О себе: <strong><?= $about ?></strong></p>
 
      </body>
   </html>
