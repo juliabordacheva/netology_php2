d6b8d6ac09e2247c2be04bb7cf3471d9eefa0f0c
Юлия Бордачева <s_proza@list.ru>
2018-12-11 17:36:37 +0300

    First Commit

---
+++ about.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Знакомство с PHP</title>
+    </head>
+    <body>
+      <h1>Знакомство с PHP</h1>
+    </body>
+  </html>
+
+
+
+<?php header("Content-Type: text/html; charset=utf-8");
+    $userName = 'Юлия';
+    $userAge = '36';
+    $mail = 'yulia@mail.ru';
+    $city = 'Moscow';
+    $about = 'Продавщица в Пятерочке';
+    ?>
+
+
+    <p>Имя: <strong><?= $userName ?></strong></p>
+    <p>Возраст: <strong><?= $userAge ?></strong></p>
+    <p>Электронная почта: <strong><?= $mail ?></strong></p>
+    <p>Город: <strong><?= $city ?></strong></p>
+    <p>О себе: <strong><?= $about ?></strong></p>
+
+     </body>
+  </html>
