1102ec86014267995f93b919cde6f66cc99605d9
Юлия Бордачева <s_proza@list.ru>
2018-12-16 22:36:41 +0300

    7commit

---
+++ DZ_ARRAY.php
@@ -0,0 +1,25 @@
+<?php
+
+$animals = ['Europe' => ['Cervus elaphus', 'Erinaceus europaeus',  'Ursus arctos', 'Sus scrofa'],
+	             'Asia' => ['Ailuropoda melanoleuca', 'Nasalis larvatus', 'Pholidota', 'Symphalangus syndactylus'],
+	             'Africa' => ['Giraffa camelopardalis', 'Rhinoceros bicornis', 'Troglodytes gorilla'],
+	             'Australia' => ['Macropus rufus', 'Ornithorhynchus anatinus', 'Vombatidae'],
+	             'India' => ['Elephas maximus', 'Panthera tigris', 'Panthera leo persica'], 
+	             'Antarctida' => ['Aptenodytes forsteri', 'Leptonychotes weddellii', 'Cephalorhynchus commersonii'],
+	             'North America' => ['Vulpes lagopus', 'Ovibos moschatus', 'Procyon lotor'],
+	             'Southern America' => ['Cacajao calvus', 'Vicugna pacos', 'Lagidium']];
+
+var_dump($animals);
+mixed array_search (mixed $' ', $animals = array $[,bool $strict = false])
+   echo as animals2=[];
+   
+shuffle($animals2);
+foreach ($word as $word) {
+    echo "$word ";
+}
+
+
+?>
+
+
+?>
