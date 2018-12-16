4de1786a6cb8f2021200f639cbf96e7c9e2a13fe
Юлия Бордачева <s_proza@list.ru>
2018-12-16 21:58:52 +0300

    6commit

--- Additional_work2.php
+++ Additional_work2.php
@@ -3,17 +3,32 @@
 
 
  <?php 
+      $userValue = rand(0,100);
+      $var1 = 1;
+      $var2 = 1;
+      
+      
 
-      $var1 = '1';
-      $var2 = '1';
-      $var3 = '1';
-      $userValue = 'User Value';
       if ($var1 > $userValue) {
       	echo 'задуманное число не входит в числовой ряд';
-      } else {
-      if ($var1=$userValue);
+      	exit;
+      } 
+       
+
+      if ($var1< $userValue) {
+      	 if ($var1==$userValue);{
         echo 'задуманное число входит в числовой ряд';
-    }
-        for ($var3 = 1, $var1 + $var2, $var2 = $var3; $var1 = 1;) continue;
+        exit;
+      } 
+      }
+      if ($var1 != $userValue) {
+      
+          for ($var1 = 1; $var2 = 1; $var1 < $userValue) {
+          	echo $var1;
+         }
+         }
         
-     ?>
+        
+        
+  ?>   
+
